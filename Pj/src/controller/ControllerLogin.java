16e42d1b12fc116adcf0de8386e3a424cdb6513f
Guilherme Barboza de Albuquerque <gui67785@gmail.com>
2023-11-15 03:49:03 -0300

    att

--- Pj/src/controller/ControllerLogin.java
+++ Pj/src/controller/ControllerLogin.java
@@ -20,24 +20,6 @@ public class ControllerLogin {
     }
     
     public void loginCliente(){
-        Cliente cliente = new Cliente(null,null,null,view.getTxtcpf().getText());
-        view.getTxtsenha().getText();
-        Conexao conexao = new Conexao();
-        try{
-            Connection conn = conexao.getConnection();
-            ClientesDAO dao = new ClientesDAO(conn);
-            ResultSet res = dao.consultar(cliente);
-            if(res.next()){
-                JOptionPane.showMessageDialog(view, "Login Feito", "Aviso", JOptionPane.INFORMATION_MESSAGE);
-                String cpf = res.getString("cpf");
-                String senha = res.getString("senha");
-//                ClienteOpcFrame viewcpc = new ClienteOpcFrame(new Cliente(cpf,senha,null,null)); 
-                 
-            }else{
-                JOptionPane.showMessageDialog(view, "Login não efetuado", "Erro", JOptionPane.ERROR_MESSAGE);
-            }
         
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(view,"Login não efetuado", "Erro", JOptionPane.ERROR_MESSAGE);
-        }   
-    }}
+    }
+}
